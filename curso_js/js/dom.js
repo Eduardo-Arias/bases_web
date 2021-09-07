45e9b3e54319846f057db9743097e7d2b0307704
Jorge.Arias <delgado.jorge.eduardo95@gmail.com>
2021-09-07 13:49:35 -0500

    Se realizan los comentarios para entender que hace el codigo

--- curso_js/js/dom.js
+++ curso_js/js/dom.js
@@ -3,13 +3,21 @@ var cajas = document.getElementsByTagName('div');
 
 console.log(cajas[0]);
 
-var primeraCaja = cajas[1];
+var primeraCaja = cajas[0];
+var segundaCaja = cajas[1];
 
-//primeraCaja.textContent = 'Esta es una caja modificada';
+// Modificando el contenido de una caja
+primeraCaja.textContent = 'Esta es una caja modificada';
 
 
+// Creando contenido
 var parrafo = document.createElement('p');
-var contenido = document.createTextNode('Hola mundo desde un nuevo nodo');
-
 parrafo.textContent = 'Texto desde javascript';
+// Agregando nuevo contenido
 primeraCaja.appendChild(parrafo);
+
+
+
+// Modificando el contenido
+var contenido = 'Hola mundo desde un nuevo nodo';
+segundaCaja.innerHTML = '<p>'+contenido+'</p>';
